1c79c6dd01ea2c423c4ac8e575fc015a08c72597
Vijayakumar Moorthy <vijay@ventunotech.com>
2020-03-18 16:51:07 +0530

    Initial Commit

---
+++ SplashScreenVideoPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashScreenVideoPOC
+//
+//  Created by Ventuno Technologies on 18/03/20.
+//  Copyright © 2020 Ventuno Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
